df3ea408ffa93044ca7fcfca0a5c34130ce03da1
john <kiokoj81@gmail.com>
2023-02-21 16:09:20 +0300

    Create User and foods

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'foods/index'
+  get 'foods/new'
+  get 'foods/edit'
+  get 'foods/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
