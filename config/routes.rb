2a185b8b1dbb0496afc23505a50a40c95f0f8640
john <kiokoj81@gmail.com>
2023-02-22 12:00:39 +0300

    Configure routes for foods

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'foods/index'
-  get 'foods/new'
-  get 'foods/edit'
-  get 'foods/show'
+
+  resources :foods, only: [:index, :new, :create, :show, :destroy]
+  root "foods#index"
+  
+  resources :users, only: [:index, :show]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
