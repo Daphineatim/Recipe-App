d8e267449c8a51af410daedd5fffe66f6814aea2
Daphineatim <hopedaphine19@gmail.com>
2023-02-22 07:40:23 -0700

    Modify files in the app folder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  protect_from_forgery with: :exception
-  
-  before_action :update_allowed_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   protected
-  def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email password])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[email password current_password])
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password])
   end
 end
