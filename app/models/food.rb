e42da7acacd42244c752671346536b5515377161
Daphineatim <hopedaphine19@gmail.com>
2023-02-22 11:10:49 -0700

    Modify files in models folder

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,9 @@
 class Food < ApplicationRecord
+  belongs_to :user
   has_many :recipe_foods, dependent: :destroy
-  belongs_to :user, foreign_key: 'user_id'
+  has_many :recipes, through: :recipe_foods
 
-  validates :name, :measurement_unit, :price, :quantity, presence: true
+  validates :name, presence: true, length: { in: 1..50 }
+  validates :measurement_unit, presence: true, length: { in: 1..20 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 1 }
 end
